8784c61246e1bf3e8de82bfc05cc2b4f17624b3f
Shailesh Appukuttan <appukuttan.shailesh@gmail.com>
2020-07-15 16:28:47 +0200

    Make changes to source repo to work with HBP seafile

---
+++ hbp_seafile/client.py
@@ -0,0 +1,118 @@
+import requests
+from seafileapi.utils import urljoin
+from seafileapi.exceptions import ClientHttpError
+from seafileapi.repos import Repos
+import re
+
+class SeafileApiClient(object):
+    """Wraps seafile web api"""
+    def __init__(self, server="https://drive.ebrains.eu", username=None, password=None, token=None):
+        """Wraps various basic operations to interact with seahub http api.
+        """
+        self.server = server
+        self.username = username
+        self.password = password
+        self._token = token
+
+        self.repos = Repos(self)
+        self.groups = Groups(self)
+
+        if token is None:
+            self._get_token()
+
+    def _check_token_valid(self):
+        url = "https://drive.ebrains.eu/api2/auth/ping/"
+        data = requests.get(url, auth=HBPAuth(self.token), verify=self.verify)
+        if data.status_code == 200:
+            return True
+        else:
+            return False
+
+    def _get_token(self):
+        """
+        HBP authentication based on _hbp_auth() in 
+        https://github.com/HumanBrainProject/hbp-validation-client)
+        """
+        base_url = "https://validation-v2.brainsimulation.eu"
+        redirect_uri = base_url + '/auth'
+        session = requests.Session()
+        # log-in page of model validation service
+        r_login = session.get(base_url + "/login", allow_redirects=False)
+        if r_login.status_code != 302:
+            raise Exception(
+                "Something went wrong. Status code {} from login, expected 302"
+                .format(r_login.status_code))
+        # redirects to EBRAINS IAM log-in page
+        iam_auth_url = r_login.headers.get('location')
+        r_iam1 = session.get(iam_auth_url, allow_redirects=False)
+        if r_iam1.status_code != 200:
+            raise Exception(
+                "Something went wrong loading EBRAINS log-in page. Status code {}"
+                .format(r_iam1.status_code))
+        # fill-in and submit form
+        match = re.search(r'action=\"(?P<url>[^\"]+)\"', r_iam1.text)
+        if not match:
+            raise Exception("Received an unexpected page")
+        iam_authenticate_url = match['url'].replace("&amp;", "&")
+        r_iam2 = session.post(
+            iam_authenticate_url,
+            data={"username": self.username, "password": self.password},
+            headers={"Referer": iam_auth_url, "Host": "iam.ebrains.eu", "Origin": "https://iam.ebrains.eu"},
+            allow_redirects=False
+        )
+        if r_iam2.status_code != 302:
+            raise Exception(
+                "Something went wrong. Status code {} from authenticate, expected 302"
+                .format(r_iam2.status_code))
+        # redirects back to model validation service
+        r_val = session.get(r_iam2.headers['Location'])
+        if r_val.status_code != 200:
+            raise Exception(
+                "Something went wrong. Status code {} from final authentication step"
+                .format(r_val.status_code))
+        config = r_val.json()
+        self._token = config['token']['access_token']
+
+    def __str__(self):
+        return 'SeafileApiClient[server=%s, user=%s]' % (self.server, self.username)
+
+    __repr__ = __str__
+
+    def get(self, *args, **kwargs):
+        return self._send_request('GET', *args, **kwargs)
+
+    def post(self, *args, **kwargs):
+        return self._send_request('POST', *args, **kwargs)
+
+    def put(self, *args, **kwargs):
+        return self._send_request('PUT', *args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        return self._send_request('delete', *args, **kwargs)
+
+    def _send_request(self, method, url, *args, **kwargs):
+        if not url.startswith('http'):
+            url = urljoin(self.server, url)
+
+        headers = kwargs.get('headers', {})
+        headers.setdefault('Authorization', 'Bearer ' + self._token)
+        kwargs['headers'] = headers
+
+        expected = kwargs.pop('expected', 200)
+        if not hasattr(expected, '__iter__'):
+            expected = (expected, )
+        resp = requests.request(method, url, *args, **kwargs)
+        if resp.status_code not in expected:
+            msg = 'Expected %s, but get %s' % \
+                  (' or '.join(map(str, expected)), resp.status_code)
+            raise ClientHttpError(resp.status_code, msg)
+
+        return resp
+
+
+class Groups(object):
+    def __init__(self, client):
+        pass
+
+    def create_group(self, name):
+        pass
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.1.1'
+__version__ = '0.1'
 
 
-setup(name='seafileapi',
+setup(name='hbp_seafile',
       version=__version__,
-      license='BSD',
-      description='Client interface for Seafile Web API',
-      author='Shuai Lin',
-      author_email='linshuai2012@gmail.com',
+      license='Apache-2.0 License',
+      description='Python client interface for HBP Collaboratory Seafile storage',
+      author='Shuai Lin, Shailesh Appukuttan',
+      author_email='linshuai2012@gmail.com, appukuttan.shailesh@gmail.com',
       url='http://seafile.com',
       platforms=['Any'],
       packages=find_packages(),
-      install_requires=['requests'],
+      install_requires=['requests', 're'],
       classifiers=['Development Status :: 4 - Beta',
                    'License :: OSI Approved :: BSD License',
                    'Operating System :: OS Independent',
