761da820e2533a4161576693d8e65b7b55a4ad32
Axel <cracky5457@protonmail.com>
2020-09-07 10:55:00 +0200

    remove hardcoded drive url from function

--- ebrains_drive/file.py
+++ ebrains_drive/file.py
@@ -1,18 +1,25 @@
 from ebrains_drive.repos import Repos
 from ebrains_drive.files import SeafFile
 
+import re
+
 def get_file_by_url(client, file_url):
     """Get a single repo associated with specified repo_url
     Example inputs:
     1) https://drive.ebrains.eu/lib/0fee1620-062d-4643-865b-951de1eee355/file/sample-latest.csv
     2) https://drive.ebrains.eu/lib/0fee1620-062d-4643-865b-951de1eee355/file/Dir1/data.json
     """
-    if "drive.ebrains.eu/lib/" in file_url:
-        repo_id = file_url.split("drive.ebrains.eu/lib/")[1].split("/")[0]
-        file_path = "/" + "/".join(file_url.split("drive.ebrains.eu/lib/")[1].split("/")[2:])
-    else:
-        raise ValueError("Parameter `file_url` does not have expected format!")
 
-    repo_obj = Repos(client).get_repo(repo_id)
+    regex = r".*\/lib\/(.*)\/file(\/.*)"
+
+    matches = re.search(regex, file_url)
+    if matches is None:
+        raise ValueError("Parameter `file_url` does not have expected format!")
+    else:
+        repo_id = matches.group(1)
+        file_path = matches.group(2)
+
+    repo_obj = client.repos.get_repo(repo_id)
     file_obj = repo_obj.get_file(file_path)
     return file_obj
+    
