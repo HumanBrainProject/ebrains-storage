83e8ded0c1ff9a2d309c01bffd81dd4efa2b1a23
Andrew Davison <andrew.davison@cnrs.fr>
2024-11-05 10:53:53 +0100

    Switch to pyproject.toml-based build
    To build source package and wheel: `python -m build`
    
    Uses setuptools_scm for versioning (version is based on git tags)

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-from setuptools import setup, find_packages
-from version_query import predict_version_str
+from setuptools import setup
 
-__version__ = predict_version_str()
-
-
-setup(name='ebrains-drive',
-      version=__version__,
-      license='Apache-2.0 License',
-      description='Python client interface for EBRAINS Collaboratory Seafile storage',
-      author='EBRAINS, CNRS',
-      author_email='support@ebrains.eu',
-      url='https://github.com/HumanBrainProject/ebrains-drive/',
-      platforms=['Any'],
-      packages=find_packages(),
-      install_requires=['requests', 'tqdm'],
-      classifiers=['Development Status :: 4 - Beta',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python'],
-      )
+setup()
